b53025052397466569a5aa99508df56af9eb1048
bleatingchef <ananddurgesh701@gmail.com>
2024-10-11 10:59:08 +0530

    404 resolve karne ki Koshish

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import vercel from 'vite-plugin-vercel';
+ 
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    port: process.env.PORT,
+  },
+  plugins: [vercel(),react()],
+});
+
